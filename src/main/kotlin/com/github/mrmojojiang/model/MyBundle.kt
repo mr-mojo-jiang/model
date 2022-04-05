f7b2da2c7299a1c15f0b733c1602a1064e62d5af
GitHub Action <action@github.com>
2022-04-05 08:28:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrmojojiang/model/MyBundle.kt
+++ src/main/kotlin/com/github/mrmojojiang/model/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mrmojojiang.model
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mrmojojiang/model/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mrmojojiang/model/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrmojojiang.model.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrmojojiang.model.MyBundle
 
 class MyApplicationService {
 
